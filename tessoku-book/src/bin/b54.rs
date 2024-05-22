2bfaf6d846abf81f42f6111f1e9b515560763f44
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2024-05-22 23:23:50 +0900

    tessoku-book

---
+++ tessoku-book/src/bin/b54.rs
@@ -0,0 +1,11 @@
+use proconio::fastout;
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        n: usize,
+        a: Chars,
+    }
+    println!("{} {:?}", n, a);
+}
